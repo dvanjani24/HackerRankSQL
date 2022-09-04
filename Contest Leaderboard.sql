9fda09081d3ab7ccdccc9111ee65326f3600d03c
Deepak <97678601+dvanjani24@users.noreply.github.com>
2022-09-04 12:19:22 -0400

    Create Contest Leaderboard.sql

---
+++ Contest Leaderboard.sql
@@ -0,0 +1,14 @@
+/* You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
+
+The total score of a hacker is the sum of their maximum scores for all of the challenges. Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score. If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. Exclude all hackers with a total score of 0 from your result.
+
+*/
+---------
+
+select m.hacker_id, h.name, sum(score) as total_score
+from (select hacker_id, challenge_id, max(score) as score from submissions
+     group by hacker_id, challenge_id) as m
+inner join hackers h on m.hacker_id = h.hacker_id
+group by m.hacker_id, h.name
+having total_score > 0
+order by total_score desc, m.hacker_id asc;
