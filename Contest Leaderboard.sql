ca7b5f5ede828a14d7176be1d55683ed323262a9
Deepak <97678601+dvanjani24@users.noreply.github.com>
2022-09-04 12:21:59 -0400

    Update Contest Leaderboard.sql

--- Contest Leaderboard.sql
+++ Contest Leaderboard.sql
@@ -1,7 +1,8 @@
 /* You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
-The total score of a hacker is the sum of their maximum scores for all of the challenges. Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score. If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. Exclude all hackers with a total score of 0 from your result.
+The total score of a hacker is the sum of their maximum scores for all of the challenges. Write a query to print the hacker_id, 
+name, and total score of the hackers ordered by the descending score. If more than one hacker achieved the same total score, 
+then sort the result by ascending hacker_id. Exclude all hackers with a total score of 0 from your result.
 */
-
 
 select m.hacker_id, h.name, sum(score) as total_score
 from (select hacker_id, challenge_id, max(score) as score from submissions
