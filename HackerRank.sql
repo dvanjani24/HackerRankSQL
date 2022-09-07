82aa4e25fc8667ae8dc70d87607bcd4e6a8bd513
Deepak <97678601+dvanjani24@users.noreply.github.com>
2022-09-06 20:48:10 -0400

    Update HackerRank.sql

--- HackerRank.sql
+++ HackerRank.sql
@@ -11,3 +11,13 @@ inner join hackers h on m.hacker_id = h.hacker_id
 group by m.hacker_id, h.name
 having total_score > 0
 order by total_score desc, m.hacker_id asc;
+
+/*
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent)
+and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+select country.continent, floor(avg(city.population))
+from country inner join city on country.code = city.countrycode
+group by country.continent;
